6c5cd691ead98665ad04ee398f93e8f8ca0e6d6a
Rostyslav Skovera <skovera@animatec.cz>
2024-04-04 10:20:20 +0200

    loginForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ToastModule} from "primeng/toast";
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import {ButtonModule} from "primeng/button";
+import {CardModule} from "primeng/card";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MessageService} from "primeng/api";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import {InputTextModule} from "primeng/inputtext";
+import { PasswordMatchDirective } from './shared/password-match.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    PasswordMatchDirective
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastModule,
+    ButtonModule,
+    CardModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    InputTextModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
